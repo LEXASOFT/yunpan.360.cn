892d07f39d764d29a4ab98adc486f368fb9791c8
Alexey Gudym <lexasoft83@gmail.com>
2014-01-07 00:51:31 +0600

    Добавил шаблоны урл Увеличил время перепроверки до 3 сек

--- yunpan.360.cn.meta.js
+++ yunpan.360.cn.meta.js
@@ -1,14 +1,18 @@
 // ==UserScript==
 // @id          yunpan.360.cn
 // @name        yunpan.360.cn
-// @version     1.1.1
+// @version     1.1.2
 // @namespace   https://github.com/LEXASOFT/yunpan.360.cn/
 // @updateURL   https://raw.github.com/LEXASOFT/yunpan.360.cn/master/yunpan.360.cn.meta.js
 // @downloadURL https://raw.github.com/LEXASOFT/yunpan.360.cn/master/yunpan.360.cn.user.js
 // @description Русский перевод сайта yunpan.360.cn
 // @include     http://yunpan.360.cn/*
 // @include     http://*.yunpan.360.cn/*
+// @include     http://yunpan.cn/*
+// @include     http://*.yunpan.cn/*
 // @match       http://yunpan.360.cn/*
 // @match       http://*.yunpan.360.cn/*
+// @match       http://yunpan.cn/*
+// @match       http://*.yunpan.cn/*
 // @grant       none
 // ==/UserScript==
--- yunpan.360.cn.user.js
+++ yunpan.360.cn.user.js
@@ -1,15 +1,19 @@
 // ==UserScript==
 // @id          yunpan.360.cn
 // @name        yunpan.360.cn
-// @version     1.1.1
+// @version     1.1.2
 // @namespace   https://github.com/LEXASOFT/yunpan.360.cn/
 // @updateURL   https://raw.github.com/LEXASOFT/yunpan.360.cn/master/yunpan.360.cn.meta.js
 // @downloadURL https://raw.github.com/LEXASOFT/yunpan.360.cn/master/yunpan.360.cn.user.js
 // @description Русский перевод сайта yunpan.360.cn
 // @include     http://yunpan.360.cn/*
 // @include     http://*.yunpan.360.cn/*
+// @include     http://yunpan.cn/*
+// @include     http://*.yunpan.cn/*
 // @match       http://yunpan.360.cn/*
 // @match       http://*.yunpan.360.cn/*
+// @match       http://yunpan.cn/*
+// @match       http://*.yunpan.cn/*
 // @grant       none
 // ==/UserScript==
 
@@ -191,7 +195,7 @@
         for(var t in ts) {
             findAndReplace(t,ts[t]);
         };
-        setTimeout(translate, 1000);
+        setTimeout(translate, 3000);
     };
 
     setTimeout(translate, 1000);
