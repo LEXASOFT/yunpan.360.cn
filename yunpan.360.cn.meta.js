fb4fe3faeeb67cd589505788f53dbae779448509
LordMerlin <ekun@mail.ru>
2014-05-19 08:31:32 -0700

    Update yunpan.360.cn.meta.js
    First release

--- yunpan.360.cn.meta.js
+++ yunpan.360.cn.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @id          yunpan.360.cn
 // @name        yunpan.360.cn
-// @version     1.1.2
-// @namespace   https://github.com/LEXASOFT/yunpan.360.cn/
-// @updateURL   https://raw.github.com/LEXASOFT/yunpan.360.cn/master/yunpan.360.cn.meta.js
-// @downloadURL https://raw.github.com/LEXASOFT/yunpan.360.cn/master/yunpan.360.cn.user.js
-// @description Русский перевод сайта yunpan.360.cn
+// @version     0.0.1
+// @namespace   https://github.com/LordMerlin/yunpan.360.cn/
+// @updateURL   https://raw.github.com/LordMerlin/yunpan.360.cn/master/yunpan.360.cn.meta.js
+// @downloadURL https://raw.github.com/LordMerlin/yunpan.360.cn/master/yunpan.360.cn.user.js
+// @description English translation of the site yunpan.360.cn
 // @include     http://yunpan.360.cn/*
 // @include     http://*.yunpan.360.cn/*
 // @include     http://yunpan.cn/*
